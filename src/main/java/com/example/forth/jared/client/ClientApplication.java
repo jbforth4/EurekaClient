108b172d67a29f4251b957c322069534768c40bd
Jared Forth <c-jared.forth@charter.com>
2017-08-25 11:34:31 -0500

    update

--- src/main/java/com/example/forth/jared/client/ClientApplication.java
+++ src/main/java/com/example/forth/jared/client/ClientApplication.java
@@ -1,17 +1,16 @@
 package com.example.forth.jared.client;
 
-import com.netflix.appinfo.InstanceInfo;
-import com.netflix.discovery.EurekaClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -19,22 +18,16 @@ import org.springframework.web.client.RestTemplate;
 public class ClientApplication {
 
 	@Autowired
-	private EurekaClient client;
-
-	@Autowired
-	private RestTemplateBuilder restTemplateBuilder;
+	private DiscoveryClient discoveryClient;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClientApplication.class, args);
 	}
 
-	@RequestMapping("/")
-	public String callService(){
-		RestTemplate restTemplate = restTemplateBuilder.build();
-		InstanceInfo instanceInfo = client.getNextServerFromEureka("service-id", false);
-		String baseUrl = instanceInfo.getHomePageUrl();
-		ResponseEntity<String> response = restTemplate.exchange(baseUrl, HttpMethod.GET, null, String.class);
-		return response.getBody();
+	@RequestMapping("/service-instances/{applicationName}")
+	public List<ServiceInstance> serviceInstancesByApplicationName(
+			@PathVariable String applicationName) {
+		return this.discoveryClient.getInstances(applicationName);
 	}
 }
 
